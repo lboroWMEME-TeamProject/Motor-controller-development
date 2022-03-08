a1359d04c6147200168bbb24b9080602e88bd264
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-08 21:31:53 +0000

    Implemented RTOS with 2 tasks

--- Motor Shield/OOP Design/main.cpp
+++ Motor Shield/OOP Design/main.cpp
@@ -1,16 +1,50 @@
 #include "myRobot.h"
-
+#include <Arduino_FreeRTOS.h>
 
  myRobot robot;
  myRobot::Encoder encoder(ENCODER1_A,ENCODER1_B);
 
+void task1(void *arg);
+void task2(void *arg);
 
-void setup()
+
+TaskHandle_t Task_Handle1;
+TaskHandle_t Task_Handle2;
+
+void setup() 
 {
  Serial.begin(9600);
-} 
-void loop()
+
+ xTaskCreate(task1,"Task1",100,NULL,1,&Task_Handle1);
+ xTaskCreate(task2,"Task2",100,NULL,1,&Task_Handle2); 
+ vTaskStartScheduler();
+}
+
+void loop() {/*DO NOT USE*/}
+
+void task1(void *arg)
 {
-  robot.move("forward",100);
-  encoder.show();
+  (void) arg;
+
+  TickType_t getTick;
+  getTick = xTaskGetTickCount();
+  
+  while(true)
+  {
+    robot.move("forward",100);
+    vTaskDelayUntil(&getTick,5000/portTICK_PERIOD_MS);
+    robot.move("reverse",100);  
+  }
+}
+
+
+void task2(void *arg)
+{
+  (void) arg;
+
+  while(true)
+  {
+    encoder.show();
+  }     
+
 }
