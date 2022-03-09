467d85f3de8d8d9265c65184e2120c7ed8bc3968
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-09 09:41:21 +0000

    Source code for relay

---
+++ Motor Shield/OOP Design/Relay.cpp
@@ -0,0 +1,29 @@
+#include "Arduino.h"
+#include "Relay.h"
+
+
+Relay::Relay(int a,int b,int c)
+{
+  pins[0]=a;
+  pins[1]=b;
+  pins[2]=c;
+
+
+  pinMode(pins[0], OUTPUT); 
+  pinMode(pins[1], OUTPUT); 
+  pinMode(pins[2], OUTPUT); 
+}
+
+void Relay::On()
+{
+  digitalWrite(pins[0], HIGH); 
+  digitalWrite(pins[1], HIGH);  
+  digitalWrite(pins[2], HIGH); 
+}
+
+void Relay::Off()
+{
+  digitalWrite(pins[0], LOW); 
+  digitalWrite(pins[1], LOW);  
+  digitalWrite(pins[2], LOW); 
+}
