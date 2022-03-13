325c1878a065dc85c13f5958b84445fc66148e32
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-13 12:49:59 +0000

    Update main.cpp

--- Motor Shield/OOP Design/main.cpp
+++ Motor Shield/OOP Design/main.cpp
@@ -1,66 +1,18 @@
 #include "myRobot.h"
-#include <Arduino_FreeRTOS.h>
 
-myRobot robot;
-myRobot::Encoder encoder(ENCODER1_A,ENCODER1_B);
-myRobot::Encoder encoder2(ENCODER2_A,ENCODER2_B);
+//myRobot robot;
+//myRobot::Encoder encoder(ENCODER1_A,ENCODER1_B);
+//myRobot::Encoder encoder2(ENCODER2_A,ENCODER2_B);
+//Relay relay(RELAY1,RELAY2,RELAY3);// replace these with global definitions or any other pins
+EncoderCounter encoder;
 
-void task1(void *arg);
-void task2(void *arg);
-void task3(void *arg);
 
-TaskHandle_t Task_Handle1;
-TaskHandle_t Task_Handle2;
-TaskHandle_t Task_Handle3;
-
-void setup() 
+void setup()
 {
- Serial.begin(9600);
-
- xTaskCreate(task1,"Task1",100,NULL,1,&Task_Handle1);
- xTaskCreate(task2,"Task2",100,NULL,1,&Task_Handle2);
- xTaskCreate(task3,"Task2",100,NULL,1,&Task_Handle3);  
+  Serial.begin (9600);  
 }
 
-void loop() {/*DO NOT USE*/}
-
-void task1(void *arg)
+void loop()
 {
-  (void) arg;
-
-  TickType_t getTick;
-  getTick = xTaskGetTickCount();
-  
-  while(true)
-  {
-    robot.move("forward",100);
-    vTaskDelayUntil(&getTick,5000/portTICK_PERIOD_MS);
-    robot.move("reverse",100);
-    vTaskDelayUntil(&getTick,5000/portTICK_PERIOD_MS);
-    robot.brake(); 
-    vTaskDelayUntil(&getTick,5000/portTICK_PERIOD_MS); 
-  }
-}
-
-
-void task2(void *arg)
-{
-  (void) arg;
-
-  while(true)
-  {
-    encoder.show();
-  }     
-
-}
-
-void task3(void *arg)
-{
-  (void) arg;
-
-  while(true)
-  {
-    encoder2.show();
-  }     
-
+  encoder.showCount();
 }
