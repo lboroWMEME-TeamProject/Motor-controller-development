8103e7a7b31b3cbb5160783699bb03a57be99439
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-23 20:14:50 +0000

    Update main.cpp

--- Motor Shield/OOP Design/main.cpp
+++ Motor Shield/OOP Design/main.cpp
@@ -1,18 +1,27 @@
 #include "myRobot.h"
 
-//myRobot robot;
-//myRobot::Encoder encoder(ENCODER1_A,ENCODER1_B);
-//myRobot::Encoder encoder2(ENCODER2_A,ENCODER2_B);
-//Relay relay(RELAY1,RELAY2,RELAY3);// replace these with global definitions or any other pins
-EncoderCounter encoder;
+myRobot robot;
+myRobot::Encoder encoder1(ENCODER1_A,ENCODER1_B);
+myRobot::Encoder encoder2(ENCODER2_A,ENCODER2_B);
 
 
 void setup()
 {
-  Serial.begin (9600);  
+  encoder_init();// initialise the encoder 
 }
 
 void loop()
+{ 
+  robot.move("forward",100);
+  delay(1000);
+  robot.move("reverse",100);
+  delay(3000);
+  
+
+}
+
+ISR(TIMER1_COMPA_vect)//timer1 interrupt 4Hz
 {
-  encoder.showCount();
+  encoder1.show();
+  encoder2.show();
 }
