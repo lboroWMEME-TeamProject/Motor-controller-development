c5acb54390f6c4ae4d3a8e2a2b0e87c38f9a71a0
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-25 10:35:18 +0000

    A working copy of the PID controller

--- Motor Shield/PID Controller/main1.cpp
+++ Motor Shield/PID Controller/main1.cpp
@@ -1,18 +1,21 @@
 #include "Control.h"
 
-myRobot robot;
-Controller pid1(0);
-Controller pid2(1);
+myRobot robot; // initialise all pins
+Controller pid1(0); // initialise PID for Motor1
+Controller pid2(1);// initialise PID for Motor2
 
 void setup()
 {
   Serial.begin(9600);
-  pid1.setCommand(true,1.75);  // set direction and distance in meters
-  pid2.setCommand(true,1.75);
+  pid1.setCommand(true,1.5); // arg1 = direction, arg2= distance in meters
+  pid1.setControl(5,0.5,0); // kp = 0.5, kd = 0.5, ki = 0
+
+  pid2.setCommand(true,1.5); 
+  pid2.setControl(5,0.5,0);  
 }
 
 void loop()
 {
-  pid1.Compute();  
+  pid1.Compute();
   pid2.Compute();
 }
