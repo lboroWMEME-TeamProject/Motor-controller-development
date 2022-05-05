b2dc0fe597d430f44eb64fb8a8d7f8a5b5430d7e
areebTP <92602684+areebTP@users.noreply.github.com>
2022-05-05 12:52:31 +0100

    Update PID_alt_speed.ino

--- Motor Shield/PID with Encoder/PID_alt_speed.ino
+++ Motor Shield/PID with Encoder/PID_alt_speed.ino
@@ -4,7 +4,7 @@ myRobot robot; // initialise all pins
 Controller pid0(0); // create PID object for Motor 0
 Controller pid1(1);// create PID object for Motor 1
 
-//myRobot::Encoder encoder1(0); // insitialise the encoder1 pins
+//myRobot::Encoder encoder1(0); // insitialise the encoder1 pins, used for printing encoder speed
 //myRobot::Encoder encoder2(1); // insitialise the encoder2 pins
 
 
@@ -18,7 +18,7 @@ void setup()
   pid1.setCommand(false,0.25); // set direction and speed for PID1
   pid1.setControl(0.30,0.0750,0.75);// set Kp,Kd and Ki
 
-//  encoder_init();  
+//  encoder_init();  function used to initialise the timer based interrupt 
 }
 
 void loop()
@@ -27,16 +27,17 @@ void loop()
 //   pid1.Compute();
 
 
- //  pid0.setAll(0.75,false,0.275,0.0850,0.55); 
+// alternate function that does not require setCommand() and setControl member functions 
+ //  pid0.setAll(0.75,false,0.275,0.0850,0.55); // arguments: target in m/s,direction,kp,kd,ki
  //  pid1.setAll(0.75,false,0.30,0.0750,0.75); 
 
 }
 
 
 /*
-ISR(TIMER1_COMPA_vect)//timer1 interrupt 4Hz, PUT THIS INSIDE MAIN
+ISR(TIMER1_COMPA_vect)//timer1 interrupt 4Hz, interrupt hander function 
 {
-//  encoder1.show();
+//  encoder1.show();// print the real-time encoder speed
 //  encoder2.show();
 }
 */
